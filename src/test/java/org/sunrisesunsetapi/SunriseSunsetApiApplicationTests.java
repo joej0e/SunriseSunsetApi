195590b95c289104aafb419779d5cb9a4b143e46
Joanna <i1johanna1s@gmail.com>
2020-03-02 14:41:36 +0200

    initial commit

---
+++ src/test/java/org/sunrisesunsetapi/SunriseSunsetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunrisesunsetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseSunsetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
